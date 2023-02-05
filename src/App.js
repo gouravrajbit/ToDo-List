27c35030e6a89be4903f315a150c6f4bc27b6d93
Gourav Raj <gourav.raj@swymcorp.com>
2023-02-05 21:31:05 +0530

    project cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './landing-page';
+import { useState } from 'react';
 
-function App() {
+function App() {  
+  const  [username, setUsername] = useState('N/A');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingPage username={username}/>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/landing-page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function LandingPage(props){
+    return (
+        <h1>{props.username}'s ToDO List</h1>
+    )
+}
